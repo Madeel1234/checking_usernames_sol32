03490392430b5211445dacedca56be86f11583c7
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 18:49:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+// Task # 32
+// Checking User names: Do the following to create a program that simulates how websites ensure that everyone has a unique 
+// username
+// Make a list of five or more usernames called new users. Make sure one or two of the new usernames are also in the current 
+//users.
+// List.
+// Loop through new users list to see if each new user name has already been used. if it has print a message that the person.
+//Will need to enter a new username. If a username has not been used, print a message saying that the username is available. 
+let current_users = ["admin", "Eric", "Aiza", "fatima", "Ali"];
+let new_users = ["admin", "fatima", "Aliza", "Haseeb", "Noor"];
+let current_users_lower = current_users.map(user => user.toLowerCase());
+for (let new_user of new_users) {
+    if (current_users_lower.includes(new_user.toLowerCase())) {
+        console.log(`sorry ${new_user},that name is taken`);
+    }
+    else {
+        console.log(`Yes ${new_user},is still in available list`);
+    }
+}
+export {};
